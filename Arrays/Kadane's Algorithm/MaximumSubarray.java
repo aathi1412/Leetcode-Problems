737647ad45b1472823700f1930d87072db889776
Aathi <160604520+aathi1412@users.noreply.github.com>
2026-05-27 15:46:55 +0530

    Create MaximumSubarray.java

---
+++ Arrays/Kadane's Algorithm/MaximumSubarray.java
@@ -0,0 +1,12 @@
+
+public int maxSubArray(int[] nums) {
+    int max = Integer.MIN_VALUE, sum = 0;
+
+    for(int n: nums){
+        sum += n;
+
+        max = Math.max(max, sum);
+        if(sum < 0) sum = 0;
+    }
+    return max;
+}
