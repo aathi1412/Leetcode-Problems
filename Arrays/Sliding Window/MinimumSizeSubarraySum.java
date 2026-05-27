f6bda983f17476c89c245b39dc8df2368c0f8423
Aathi <160604520+aathi1412@users.noreply.github.com>
2026-05-27 15:38:35 +0530

    Add method to find minimum size subarray sum
    Implement a method to find the minimum size subarray sum that meets or exceeds a target value.

---
+++ Arrays/Sliding Window/MinimumSizeSubarraySum.java
@@ -0,0 +1,17 @@
+
+public int minSubArrayLen(int target, int[] nums) {
+    int left=0, right=0, min=Integer.MAX_VALUE, sum=0;
+
+    while(right < nums.length){
+        sum += nums[right];
+
+        while(sum >= target){
+            min = Math.min(min, right-left+1);
+            sum -= nums[left];
+            left++;
+        }
+        
+        right++;
+    }
+    return min == Integer.MAX_VALUE ? 0 : min;
+}
