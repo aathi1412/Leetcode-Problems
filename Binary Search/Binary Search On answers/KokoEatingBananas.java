99967f25b0749cef6c4cc7f34a5a6f003db2280f
Aathi <160604520+aathi1412@users.noreply.github.com>
2026-05-29 23:47:00 +0530

    Implement minEatingSpeed method for Koko's problem

--- Binary Search/Binary Search On answers/KokoEatingBananas.java
+++ Binary Search/Binary Search On answers/KokoEatingBananas.java
@@ -1,1 +1,20 @@
 
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 0, ans = right;
+
+        for(int m: piles) right = Math.max(right, m);
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            long hours = 0;
+            for(int n: piles){
+                hours += (n + mid - 1) / mid;
+            }
+            if(hours <= h){
+                ans = mid;
+                right = mid -1;
+            }
+            else left = mid + 1;
+        }
+        return ans;
+    }
