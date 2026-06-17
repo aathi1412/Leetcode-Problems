aff45a1d05dd35e047a85cfd87c37a7327f086da
Aathi <aathithirunav@gmail.com>
2026-06-17 21:33:59 +0530

    add FindSortOrNot class to determine if an array is sorted using recursion

---
+++ sort/FindSortOrNot.java
@@ -0,0 +1,14 @@
+package Arrays.sort;
+
+public class FindSortOrNot {
+    public static void main(String[] args) {
+        int[] n = {1,2,3,4,5,0};
+        System.out.println(sort(n, 0));
+    }   
+
+    static boolean sort(int[] n, int i){
+        if(i == n.length) return true;
+        if(i > 0 && n[i] < n[i-1]) return false;
+        return sort(n, ++i);
+    }
+}
